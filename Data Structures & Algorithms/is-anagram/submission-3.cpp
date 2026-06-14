0510895f9fcd0dd6e7186a8d5332c026ae10ae96
C.V.Jaswanthi Reddy <138797809+CVJaswanthiReddy@users.noreply.github.com>
2026-06-14 11:40:39 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>ans(26,0);
+        int n=s.length();
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            int c= s[i]-'a';
+            ans[c]++;
+            int d=t[i]-'a';
+            ans[d]--;
+        }
+        for(int i=0;i<ans.size();i++){
+            if(ans[i] >=1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
